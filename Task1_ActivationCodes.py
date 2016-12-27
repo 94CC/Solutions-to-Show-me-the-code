1164f125a42c0871c804e54fb68e2742b9314d71
Y-Cao <caoyijing94@gmail.com>
2016-12-27 20:15:48 +0000

    Update Task1_ActivationCodes.py

--- Task1_ActivationCodes.py
+++ Task1_ActivationCodes.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Task0001: generate m number of unique activation codes of length n 
 """
@@ -6,20 +5,32 @@ from string import ascii_letters
 from itertools import chain
 import numpy as np
 
-def generateACode():
-    codeList=np.random.choice(pool,length).tolist()
-    return ''.join(codeList)
+class Activ_code:
+    def __init__(self,length,number):
+        """length: length of each code
+        number: number of codes to be generated"""
+        self.length=length
+        self.number=number
+        self.pool=list(chain(range(0,10),ascii_letters)) #create a pool of characters to choose from
+        
+    def generateACode(self):
+        codeList=np.random.choice(self.pool,self.length).tolist()
+        return ''.join(codeList)
 
-def addCode():
-    codes=set() # ensure all codes are unique.
-    while len(codes)<number:
-        ACode=generateACode()
-        codes.add(ACode)
-    return codes
+    def addCode(self):
+        codes=set() # ensure all codes are unique.
+        while len(codes)<self.number:
+            ACode=self.generateACode()
+            codes.add(ACode)
+        return codes
 
-if __name__=='__main__':
-    pool=list(chain(range(0,10),ascii_letters))   #create a pool of characters to choose form
-    length=int(input('Enter the length of the activtation code: '))
+def main():
+    length=int(input('Enter the length of the activtation code(characters): '))
     number=int(input('Enter the total number of activation code to be generated: '))
-    codes=addCode()
+    MyActiv_code=Activ_code(length,number)
+    codes=MyActiv_code.addCode()
     print (codes)
+    return codes
+    
+if __name__=='__main__':
+    main()
